f9a4b77bff1858a6e8bb123603031b5be81213a2
alik321 <aliar.ulanov@gmail.com>
2021-11-13 17:00:54 +0600

    Make a Items for order!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ItemsComponent } from './items/items.component';
+import { OrdersComponent } from './orders/orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule
